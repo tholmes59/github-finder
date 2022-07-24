c86be01d1cc1148fc9bd30c14a87af9a456e2a3f
Thomas <tholmes59@gmail.com>
2022-07-24 16:25:16 -0700

    create user results component, fetch users data from github api, import spinner component, and display user login information

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {useEffect, useState} from 'react'
+import Spinner from '../layout/Spinner'
+
+function UserResults() {
+const [users, setUsers] = useState([])
+const [loading, setLoading] = useState([true])
+
+useEffect(() => {
+    fetchUsers()
+}, [])
+const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`)
+    const data = await response.json()
+    setUsers(data)
+    setLoading(false)
+}
+
+if(!loading) {
+    return (
+    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3'>
+        {users.map((user) => (
+            <h3>{user.login}</h3>
+        ))}  
+    </div>
+    )
+    } else {
+       return <Spinner/>
+    }
+}
+
+export default UserResults
