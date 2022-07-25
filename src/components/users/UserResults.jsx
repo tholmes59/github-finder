dafddeba202c30a5f06bf60e8d79759bda5b6efb
Thomas <tholmes59@gmail.com>
2022-07-25 14:25:22 -0700

    move state and fetch users function to context file and import this context file

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,33 +1,27 @@
 import React from 'react'
-import {useEffect, useState} from 'react'
+import {useEffect, useContext} from 'react'
 import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
+import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
-const [users, setUsers] = useState([])
-const [loading, setLoading] = useState([true])
+    const {users, loading, fetchUsers} = useContext(GithubContext)
 
-useEffect(() => {
-    fetchUsers()
-}, [])
-const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`)
-    const data = await response.json()
-    setUsers(data)
-    setLoading(false)
-}
+    useEffect(() => {
+        fetchUsers()
+    }, [])
 
-if(!loading) {
-    return (
-    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3'>
-        {users.map((user) => (
-            <UserItem key={user.id} user={user} />
-        ))}  
-    </div>
-    )
-    } else {
-       return <Spinner/>
-    }
+    if(!loading) {
+        return (
+        <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3'>
+            {users.map((user) => (
+                <UserItem key={user.id} user={user} />
+            ))}  
+        </div>
+        )
+        } else {
+        return <Spinner/>
+        }
 }
 
 export default UserResults
