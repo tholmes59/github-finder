6fc1b669d3b9ad9effa6180820ab70e112b96343
Thomas <tholmes59@gmail.com>
2022-07-26 08:42:54 -0700

    remove use effect hook from component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import {useEffect, useContext} from 'react'
+import {useContext} from 'react'
 import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
 import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
-    const {users, loading, fetchUsers} = useContext(GithubContext)
-
-    useEffect(() => {
-        fetchUsers()
-    }, [])
+    const {users, loading} = useContext(GithubContext)
 
     if(!loading) {
         return (
