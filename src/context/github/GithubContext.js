a1495d8079bd38db8afe1acdad8874ccba233c7e
Thomas <tholmes59@gmail.com>
2022-07-28 11:34:38 -0700

    create get user function to get individual user data based on user login params

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,6 +8,7 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
+        user: {},
         loading: false
     }
 
@@ -27,6 +28,23 @@ export const GithubProvider = ({children}) => {
         })
     }
 
+    //Get single user
+    const getUser = async (login) => {
+        setLoading()
+       
+        const response = await fetch(`${GITHUB_URL}/users/${login}`)
+
+        if(response.status === 404){
+            window.location = '/notfound'
+        } else {
+            const data = await response.json()
+            dispatch({
+                type: 'GET_USER',
+                payload: data
+            })
+        }
+    }
+
     //Clear Users
     const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
 
@@ -36,8 +54,10 @@ export const GithubProvider = ({children}) => {
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
         searchUsers,
-        clearUsers
+        clearUsers,
+        getUser
     }}>
         {children}
     </GithubContext.Provider>
