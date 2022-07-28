c30042024af8497f59133a4e42b353d96766c76f
Thomas <tholmes59@gmail.com>
2022-07-28 08:23:14 -0700

    import alert into App.js and place it above the search bar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import {GithubProvider} from './context/github/GithubContext'
 import {AlertProvider} from './context/alert/AlertContext'
+import Alert from './components/layout/Alert';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <div className='flex flex-col justify-between h-screen'>
             <Navbar/>
             <main className='container mx-auto px-3 pb-12'>
+              <Alert/>
               <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/about' element={<About/>}/>
