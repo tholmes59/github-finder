0424b721b5b9f3b91201ac21053218341ddc18db
Thomas <tholmes59@gmail.com>
2022-08-02 12:53:22 -0700

    move get user and get user repos to github actions file

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -12,3 +12,30 @@ export const searchUsers = async (text) => {
     const {items} = await response.json()
     return items
 }
+
+//Get single user
+export const getUser = async (login) => {
+   
+    const response = await fetch(`${GITHUB_URL}/users/${login}`)
+
+    if(response.status === 404){
+        window.location = '/notfound'
+    } else {
+        const data = await response.json()
+        return data
+    }
+}
+
+//User repos results
+export const getUserRepos = async (login) => {
+
+    const params = new URLSearchParams({
+        sort: 'created',
+        per_page: 10
+
+    })
+    
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`)
+    const data = await response.json()
+    return data
+}
