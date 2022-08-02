e5331f12bb1ce5bebb9ab4d698edd6ed8fb481ec
Thomas <tholmes59@gmail.com>
2022-08-02 12:52:25 -0700

    move get user and get user repos functions to github actions file and remove them from the provider values. Also remove clear users and set loading as these will be dispatched in the user and user search components

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,10 +1,7 @@
 import { createContext, useReducer} from 'react'
-import { createRenderer } from 'react-dom/test-utils'
 import githubReducer from './GithubReducer'
 
 const GithubContext = createContext()
-
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 
 export const GithubProvider = ({children}) => {
     const initialState = {
@@ -16,55 +13,9 @@ export const GithubProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
-    
-
-    //Get single user
-    const getUser = async (login) => {
-        setLoading()
-       
-        const response = await fetch(`${GITHUB_URL}/users/${login}`)
-
-        if(response.status === 404){
-            window.location = '/notfound'
-        } else {
-            const data = await response.json()
-            dispatch({
-                type: 'GET_USER',
-                payload: data
-            })
-        }
-    }
-
-    //User repos results
-    const getUserRepos = async (login) => {
-        setLoading()
-
-        const params = new URLSearchParams({
-            sort: 'created',
-            per_page: 10
-
-        })
-        
-        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`)
-        const data = await response.json()
-        dispatch({
-            type: 'GET_REPOS',
-            payload: data
-        })
-    }
-
-    //Clear Users
-    const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
-
-    //Set loading
-    const setLoading = () => dispatch({type: 'SET_LOADING'})
-
     return <GithubContext.Provider value={{
         ...state,
         dispatch,
-        clearUsers,
-        getUser,
-        getUserRepos
     }}>
         {children}
     </GithubContext.Provider>
