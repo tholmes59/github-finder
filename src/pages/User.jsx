a4ebdc0d79b676a8fa718e8ccd85428d1ef8a7f3
Thomas <tholmes59@gmail.com>
2022-08-03 14:27:37 -0700

    add get user function to useEffect hook with bith user data and user repos state

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,17 +5,25 @@ import {useEffect, useContext} from 'react'
 import GithubContext from '../context/github/GithubContext'
 import Spinner from '../components/layout/Spinner'
 import RepoList from '../components/repos/RepoList'
+import {getUser, getUserRepos} from '../context/github/GithubActions'
 
 function User() {
 
-    const {getUser, user, loading, getUserRepos, repos} = useContext(GithubContext)
+    const {user, loading, repos, dispatch} = useContext(GithubContext)
 
     const params = useParams()
 
     useEffect(() => {
-        getUser(params.login)
-        getUserRepos(params.login)
-    },[])
+        dispatch({type: 'SET_LOADING'})
+        const getUserData = async() => {
+          const userData = await getUser(params.login)
+          dispatch({type: 'GET_USER', payload: userData})
+
+          const userRepos = await getUserRepos(params.login)
+          dispatch({type: 'GET_REPOS', payload: userRepos})
+        }
+        getUserData()
+    },[dispatch, params.login])
 
     const {
         name,
