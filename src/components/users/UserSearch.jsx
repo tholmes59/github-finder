13a441c1a886c75f66c31ec523de6e20770190ff
Thomas <tholmes59@gmail.com>
2022-08-03 14:28:57 -0700

    remove clear users function from context and add dispatch clear users to onclick event in clear users button

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -7,7 +7,7 @@ import {searchUsers} from '../../context/github/GithubActions'
 function UserSearch() {
     const [text, setText] = useState('')
 
-    const {users, dispatch, clearUsers} = useContext(GithubContext)
+    const {users, dispatch} = useContext(GithubContext)
     const {setAlert} = useContext(AlertContext)
 
     const handleChange = (e) => setText(e.target.value)
@@ -49,7 +49,7 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-            <button className="btn btn-ghost btn-large" onClick={clearUsers}>Clear</button>
+            <button className="btn btn-ghost btn-large" onClick={() => dispatch({type: 'CLEAR_USERS'})}>Clear</button>
         </div>
       )}
     </div>
